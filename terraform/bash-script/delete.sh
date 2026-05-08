730063333fb1bce6c5854f896e7ac03c9b4181a7
jawwad78 <ja66730@gmail.com>
2026-05-08 12:09:43 +0100

    fix: bash script text from alb to nlb

--- terraform/bash-script/delete.sh
+++ terraform/bash-script/delete.sh
@@ -3,5 +3,5 @@
 #connect to eks cluster 
 aws eks update-kubeconfig --region eu-west-2  --name eks-cluster
 
-# delete svc of traefik for alb to get destroyed
+# delete svc of traefik for nlb to get destroyed
 kubectl delete svc traefik -n traefik
